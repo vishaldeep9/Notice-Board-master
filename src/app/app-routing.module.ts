9c008cf15823f0223a7d899015cdc0cec717e574
vishaldeep09 <09vishaldeep@gmail.com>
2024-09-17 11:24:43 +0530

    move redirect path from post-module.routing.ts to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './post/create/create.component';
 
 const routes: Routes = [
-    {
+  { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
+   {
     path:'post',
     loadChildren:()=>import('./post/post.module').then(p=>p.PostModule)
    },
 ];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -6,7 +6,6 @@ import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
   
 const routes: Routes = [
-  { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
   { path: 'post/index', component: IndexComponent },
   { path: 'post/:postId/view', component: ViewComponent },
   { path: 'post/create', component: CreateComponent },
