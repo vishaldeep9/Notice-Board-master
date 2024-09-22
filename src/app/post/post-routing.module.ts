97f53bf57c5a54efb800a24692573d320aeaefc0
vishaldeep09 <09vishaldeep@gmail.com>
2024-09-22 18:48:49 +0530

    notify guard added during edit and create form

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -3,16 +3,25 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 import { CreateComponent } from './create/create.component';
-  
+import { NotifyGuard } from '../service/guards/notify.guard';
+
 const routes: Routes = [
   { path: 'post/index', component: IndexComponent },
   { path: 'post/:postId/view', component: ViewComponent },
-  { path: 'post/create', component: CreateComponent },
-  { path: 'post/:postId/edit', component: CreateComponent } 
+  {
+    path: 'post/create',
+    component: CreateComponent,
+    canDeactivate: [NotifyGuard],
+  },
+  {
+    path: 'post/:postId/edit',
+    component: CreateComponent,
+    canDeactivate: [NotifyGuard],
+  },
 ];
-  
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PostRoutingModule { }
+export class PostRoutingModule {}
